bef4578fe6a6cbd1510b0e539a1ecc252a78cf47
Nayan Buha <nayanbuha900@gmail.com>
2021-09-13 15:35:04 +0530

    Insert data in hobby table

---
+++ insertHobby.sql
@@ -0,0 +1,12 @@
+INSERT INTO hobby(id,NAME)
+VALUE
+	('1','CRICLET'),
+	('2','CRICKET'),
+	('3','Dancing'),
+	('4','Singing'),
+	('5','Cycling'),
+	('6','Exercising'),
+	('7','Drawing'),
+	('8','Painting'),
+	('9','Cooking'),
+	('10',' Walking');
