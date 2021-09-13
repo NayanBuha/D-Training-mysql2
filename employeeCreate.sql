f57f1d717cf374379c95335ba3fe7d818391d066
Nayan Buha <nayanbuha900@gmail.com>
2021-09-13 15:33:26 +0530

    create employee table

---
+++ employeeCreate.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `customer`.`employee` (  
+  `Id` INT NOT NULL,
+  `first_name` VARCHAR(250),
+  `last_name` VARCHAR(250),
+  `age` INT(20),
+  `mobile_number` INT(20),
+  `address` VARCHAR(250),
+  PRIMARY KEY (`Id`) 
+);
